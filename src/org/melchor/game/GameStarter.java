c027f4f56510eef73fdbc9cf5b51fe5e16f9e696
hypernova1 <hypemova@gmail.com>
2020-11-12 23:40:10 +0900

    숫자 생성기 클래스 생성

---
+++ src/org/melchor/game/GameStarter.java
@@ -0,0 +1,11 @@
+package org.melchor.game;
+
+public class GameStarter {
+
+    public static void main(String[] args) {
+
+        int[] numbers = NumberGenerator.generate(3);
+
+    }
+
+}
---
+++ src/org/melchor/game/NumberGenerator.java
@@ -0,0 +1,33 @@
+package org.melchor.game;
+
+public class NumberGenerator {
+
+    public static int[] generate(int size) {
+        int[] numbers = new int[size];
+        int pushedNumberCount = 0;
+        while (pushedNumberCount == size - 1) {
+            int newNumber = creteRandomNumber();
+            if (isContain(numbers, newNumber)) {
+                continue;
+            }
+            numbers[pushedNumberCount] = newNumber;
+            pushedNumberCount++;
+        }
+
+        return numbers;
+    }
+
+    private static int creteRandomNumber() {
+        return (int) (Math.random() * 9) + 1;
+    }
+
+    private static boolean isContain(int[] numbers, int newNumber) {
+        for (int number : numbers) {
+            if (number == newNumber) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+}
